45f87f60ac77eb8d76a0219e00e0f2d9425fb629
ahmedsat <ahmedsat303@gmail.com>
2022-05-04 22:48:37 +0200

    Service init

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,16 +1,23 @@
+const workoutService = require("../services/workoutService");
+
 getAllWorkouts = (req, res) => {
+  const allWorkouts = workoutService.getAllWorkouts();
   res.send("Get all workouts");
 };
 getWorkout = (req, res) => {
+  const workout = workoutService.getWorkout();
   res.send("Get a workout" + req.params.id);
 };
 createWorkout = (req, res) => {
+  const createdWorkout = workoutService.createWorkout();
   res.send("Create a workout");
 };
 updateWorkout = (req, res) => {
+  const updatedWorkout = workoutService.updateWorkout();
   res.send("Update a workout");
 };
 deleteWorkout = (req, res) => {
+  const deletedWorkout = workoutService.deleteWorkout();
   res.send("Delete a workout");
 };
 module.exports = {
---
+++ src/services/workoutService.js
@@ -0,0 +1,23 @@
+getAllWorkouts = () => {
+  return;
+};
+getWorkout = () => {
+  return;
+};
+createWorkout = () => {
+  return;
+};
+updateWorkout = () => {
+  return;
+};
+deleteWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getWorkout,
+  createWorkout,
+  updateWorkout,
+  deleteWorkout,
+};
